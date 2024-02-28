dd0fd7d5c816c054ff41433eeaee9f53bfd2ee46
Nova72Scotia <jamesscottferg@gmail.com>
2024-02-27 23:13:17 -0600

    Replaced urlfilter with JSON logic and started on scaling time system

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,5 @@
-async function send_notification() {
-    let current_counter = null;
-    current_counter = await chrome.storage.local.get("counter")
-    if (JSON.stringify(current_counter) === "{}") {
-        current_counter = 0;
-    } else {
-        current_counter = current_counter.counter;
-        current_counter = current_counter + 1;
-    }
-    chrome.storage.local.set({"counter": current_counter});
-    chrome.notifications.create('test', {
+async function send_notification(current_counter) {
+        chrome.notifications.create('test', {
         type: 'basic',
         iconUrl: 'images/temp_icon.png',
         title: 'Chump',
@@ -30,19 +21,35 @@ async function create_alarm() {
 create_alarm();
 
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-    send_notification();
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+    let current_counter = null;
+    current_counter = await chrome.storage.local.get("counter")
+    if (JSON.stringify(current_counter) === "{}") {
+        current_counter = 0;
+    } else {
+        current_counter = current_counter.counter;
+        current_counter = current_counter + 1;
+    }
+    chrome.storage.local.set({"counter": current_counter});
+    send_notification(current_counter);
 });
 
-chrome.webNavigation.onCommitted.addListener((result) => {
-    chrome.notifications.create('test2', {
-        type: 'basic',
-        iconUrl: 'images/temp_icon.png',
-        title: 'Chump',
-        message: `You navigated to ${result.url}`,
-        priority: 1
-    });
-    chrome.tabs.update({url: "site/home.html"});
-},
-{url: [{hostContains: 'facebook'}, {hostContains: 'wikipedia'}]}
-);
+chrome.webNavigation.onCompleted.addListener(async (result) => {
+    let categories = {"test": ["wikipedia", "facebook"]};
+    let censored = false;
+    for (const ele of result.url.split(".")) {
+        if (categories.test.includes(ele)) {
+            censored = true;
+        }
+    }
+    if (censored) {
+        chrome.notifications.create('test2', {
+            type: 'basic',
+            iconUrl: 'images/temp_icon.png',
+            title: 'Chump',
+            message: `You navigated to ${result.url}`,
+            priority: 1
+        });
+        chrome.tabs.update({url: "site/home.html"});
+    }
+});
