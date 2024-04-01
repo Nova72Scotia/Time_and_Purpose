022dbe35d743720fb441bd14a1cdbc3d944b6c9c
Nova72Scotia <jamesscottferg@gmail.com>
2024-03-31 20:25:25 -0500

    added existing settings loading into settings

--- options/options.js
+++ options/options.js
@@ -7,8 +7,34 @@ document.querySelector("#form").addEventListener("submit", async (event) => {
     event.preventDefault();
     let social_media_timer = (await chrome.storage.local.get("social_media_timer")).social_media_timer;
     let streaming_timer = (await chrome.storage.local.get("streaming_timer")).streaming_timer;
-    //console.log(await document.querySelector("#social_media").value);
     social_media_timer.restriction_type = document.querySelector("#social_media").value;
     streaming_timer.restriction_type = document.querySelector("#streaming").value;
     await chrome.storage.local.set({"social_media_timer": social_media_timer, "streaming_timer": streaming_timer});
 });
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let text_value_key = {"none": "None", "timed":"Partial", "restricted":"Complete"}
+    console.log("loaded");
+    let social_media_timer = (await chrome.storage.local.get("social_media_timer")).social_media_timer;
+    let streaming_timer = (await chrome.storage.local.get("streaming_timer")).streaming_timer;
+    let social_media_select = document.querySelector("#social_media");
+    for (let i = 0; i < social_media_select.length; i++) {
+        console.log(i);
+        let current_option = social_media_select[i];
+        if (current_option.value == social_media_timer.restriction_type) {
+            current_option.selected = true;
+        }
+    }
+    let streaming_select = document.querySelector("#streaming");
+    for (let i = 0; i < streaming_select.length; i++) {
+        console.log(i);
+        let current_option = streaming_select[i];
+        if (current_option.value == streaming_timer.restriction_type) {
+            current_option.selected = true;
+        }
+    }
+/*    document.querySelector("#social_media").value = social_media_timer.restriction_type;
+    document.querySelector("#social_media").textContent = text_value_key[social_media_timer.restriction_type];
+    document.querySelector("#streaming").value = streaming_timer.restriction_type;
+    document.querySelector("#streaming").textContent = text_value_key[streaming_timer.restriction_type];
+*/});
